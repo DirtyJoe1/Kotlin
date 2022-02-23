61239b61ac1497e43a0fba93e2a867af6cbb2560
RuslanOvtin <gethardrekt@mail.ru>
2022-02-23 14:19:03 +0300

    Исправил

--- Dz2/src/main/kotlin/Main.kt
+++ Dz2/src/main/kotlin/Main.kt
@@ -1,9 +1,25 @@
 fun main(){
-    val ageArray = arrayOf(18, 21, 19, 34, 40)
-    val nameArray = arrayOf("Михаил", "Иван", "Пётр", "Сергей", "Константин")
-    val surNameArray = arrayOf("Иванов", "Петров", "Мишулин", "Смирнов", "Девятаев")
+    var n = 0
+    val ageArray = Array(5){0}
+    val nameArray = Array<String?>(5){""}
+    val surnameArray = Array<String?>(5){""}
+    while(n < 5){
+        println("Введите возраст, фамилию и имя")
+        val age : Int = readLine()?.toInt()!!
+        val surname = readLine()
+        val name = readLine()
+        if ((age > 14) && (age < 18)) println("$name $surname не пускать в торговый центр")
+        if (age <= 14) println("$name $surname вызвать полицию")
+        if (age >= 18){
+            println("$name $surname пропустить")
+            ageArray[n] = age
+            nameArray[n] = name
+            surnameArray[n] = surname
+            n++
+        }
+    }
     ageArray.sort()
-    for ((i, ages) in ageArray.withIndex()) {
-        println("Фамилия, Имя, Возраст: ${ageArray[i]} ${nameArray[i]} ${surNameArray[i]}")
+    for((i) in ageArray.withIndex()) {
+        println("Возраст, Имя, Фамилия: ${ageArray[i]} ${nameArray[i]} ${surnameArray[i]}")
     }
 }
